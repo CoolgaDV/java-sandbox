19a9745068604381f2ca811e4fedba4b972e4af7
Dmitry Kulga <coolgadv@gmail.com>
2023-01-07 14:43:05 +0300

    add sample for switch expression

---
+++ src/main/java/cdv/sandbox/SwitchExpression.java
@@ -0,0 +1,28 @@
+package cdv.sandbox;
+
+import java.util.stream.IntStream;
+
+public class SwitchExpression {
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 5)
+                .mapToObj(SwitchExpression::convert)
+                .forEach(System.out::println);
+    }
+
+    private static String convert(int value) {
+        return switch (value) {
+            case 1 -> "one";
+            case 2 -> {
+                System.out.println("...");
+                // Return statement is not allowed
+                yield "couple";
+            }
+            // Several options are possible for the same branch
+            case 3, 4 -> "few";
+            // Default branch is needed
+            default -> String.valueOf(value);
+        };
+    }
+
+}
